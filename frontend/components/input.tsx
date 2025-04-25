7d79a6ad978c941058b3146dabae6a18f5f040cf
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-24 21:04:45 -0700

    created input field

--- frontend/components/input.tsx
+++ frontend/components/input.tsx
@@ -1,10 +1,30 @@
 import { CircleArrowDown } from "lucide-react";
+import { useState } from "react";
 
 const Input = () => {
+  const [inputText, setInputText] = useState("");
+  const [sumamry, setSummary] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try{
+      const response = await fetch()
+    }
+  };
+
   return (
     <div className="text-center text-2xl flex flex-col items-center justify-center h-screen">
-      <p>Article Goes Here</p>
-      <CircleArrowDown/>
+      <div className="flex items-center gap-2">
+        <p>Article Goes Here</p>
+        <CircleArrowDown />
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <textarea placeholder="Blah blah blah...." value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+          <button type="submit">Summarize Please</button>
+        </form>
+      </div>
     </div>
   );
 };
