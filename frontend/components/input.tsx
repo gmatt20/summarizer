d1507aad075bf61655036eab4ff7f736ba647cb5
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-26 19:37:34 -0700

    integrated frontend to backend

--- frontend/components/input.tsx
+++ frontend/components/input.tsx
@@ -1,15 +1,37 @@
+"use client";
+
 import { CircleArrowDown } from "lucide-react";
 import { useState } from "react";
 
 const Input = () => {
   const [inputText, setInputText] = useState("");
-  const [sumamry, setSummary] = useState("");
+  const [summary, setSummary] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    try{
-      const response = await fetch()
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:8000/summarizethisplease",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userInput: inputText }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP ERROR STATUS: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      setSummary(responseData);
+      console.log(summary);
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
   };
 
@@ -21,7 +43,11 @@ const Input = () => {
       </div>
       <div>
         <form onSubmit={handleSubmit}>
-          <textarea placeholder="Blah blah blah...." value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+          <textarea
+            placeholder="Blah blah blah...."
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
           <button type="submit">Summarize Please</button>
         </form>
       </div>
